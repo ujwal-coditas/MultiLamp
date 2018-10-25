26dbee3c3a1e9bed61b2336079e5c73679d49923
Ujwal Thote <thoteujwal97@gmail.com>
2018-10-25 13:28:17 +0530

    Optimised code

--- multilamp/src/main/java/com/coderfolk/multilamp/customView/MultiLamp.java
+++ multilamp/src/main/java/com/coderfolk/multilamp/customView/MultiLamp.java
@@ -54,97 +54,57 @@ public class MultiLamp extends RelativeLayout {
         init();
     }
 
-    public MultiLamp(Context context, @Nullable AttributeSet attrs) {
+    public MultiLamp(Context context, Activity activity, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        this.activity = activity;
         this.context = context;
         init();
     }
 
-    public MultiLamp(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public MultiLamp(Context context, Activity activity, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        this.activity = activity;
         this.context = context;
         init();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    public MultiLamp(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+    public MultiLamp(Context context, Activity activity, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        this.activity = activity;
+        this.context = context;
         init();
     }
 
     private void init() {
         density = context.getResources().getDisplayMetrics().density;
         list = new ArrayList<>();
-        mBackgroundPaint = new Paint();
-        mBackgroundPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
-        mBackgroundPaint.setTextSize(118f);
-        mBackgroundPaint.setTypeface(Typeface.create("Arial", Typeface.BOLD));
         setLayerType(LAYER_TYPE_HARDWARE, null);
     }
 
     @SuppressLint("ClickableViewAccessibility")
     public void build(ArrayList<Target> list) {
         this.list = list;
+
 //        bringToFront();
         setWillNotDraw(false);
         this.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT));
 
-//        ViewGroup.LayoutParams params=new ViewGroup.LayoutParams()
-//        this.setLayoutParams();
         this.setClickable(false);
         if (null != this.getParent()) {
             ((ViewGroup) ((Activity) activity).getWindow().getDecorView()).removeView(this);
         }
-//        WindowManager.LayoutParams params5 = new WindowManager.LayoutParams(
+        //todo need to work on it
+        //        WindowManager.LayoutParams params5 = new WindowManager.LayoutParams(
 //                WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
 //                WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
 //                PixelFormat.TRANSLUCENT);
 
         ((ViewGroup) activity.getWindow().getDecorView()).addView(this);
 
-//        Button button = new Button(this.getContext());
-//        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-//        button.setLayoutParams(params);
-//        button.setText("Got it");
-//        button.setTextColor(Color.WHITE);
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-//            button.setBackground(getResources().getDrawable(R.drawable.bg_button));
-//        } else {
-//            button.setBackgroundColor(getResources().getColor(R.color.colorAccent));
-//        }
-//        final View view3 = this;
-//
-//        this.addView(button);
-//        RelativeLayout.LayoutParams params2 = (RelativeLayout.LayoutParams) button.getLayoutParams();
-//        params2.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-//        params2.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-//        params2.setMargins(0, 0, 10 * ((int) density), 10 * ((int) density));
-//        button.setLayoutParams(params2);
-//
-////        button.setOnClickListener(new OnClickListener() {
-////            @Override
-////            public void onClick(View view) {
-////                Toast.makeText(context, "REMOVING", Toast.LENGTH_SHORT).show();
-////                view3.setVisibility(View.GONE);
-////            }
-////        });
-////        final Callback callback = (Callback) this;
-//        button.setOnTouchListener(new OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View view, MotionEvent motionEvent) {
-////                view3.setVisibility(View.GONE);
-//                ((ViewGroup) ((Activity) activity).getWindow().getDecorView()).removeView(view3);
-//                if (null != callback)
-//                    callback.onTutorialFinished();
-//                return false;
-//            }
-//        });
-//        button.setOnClickListener(this);
         if (null != list && list.size() > 0) {
             invalidate();
-//            Toast.makeText(context, "Inside OVERLAY", Toast.LENGTH_SHORT).show();
-//            Log.d("INSIDE", String.valueOf(this.get))
         }
     }
 
